eed94dd73bc7b72a0538276319f24678a75b2d3b
Xiao <liu.xiao@me.com>
2015-11-20 15:18:23 +0000

    working version, refactoring needed.

---
+++ blog/app/controllers/users/sessions_controller.rb
@@ -0,0 +1,17 @@
+class Users::SessionsController < Devise::SessionsController
+# before_filter :configure_sign_in_params, only: [:create]
+
+  # GET /resource/sign_in
+  # def new
+  #   super
+  # end
+
+  # POST /resource/sign_in
+  # DELETE /resource/sign_out
+  # protected
+
+  # If you have extra params to permit, append them to the sanitizer.
+  # def configure_sign_in_params
+  #   devise_parameter_sanitizer.for(:sign_in) << :attribute
+  # end
+end
